ec5b3133e3def82ea3f91c91589ea1f3b4598d3e
Si Jie <sijie123@gmail.com>
2018-06-15 15:35:53 +0700

    First commit with manifest

---
+++ data.js
@@ -0,0 +1,7 @@
+chrome.storage.local.set({key: value}, function() {
+  console.log('Value is set to ' + value);
+});
+
+chrome.storage.local.get(['key'], function(result) {
+  console.log('Value currently is ' + result.key);
+});
